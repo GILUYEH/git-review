9d9e14dc2773f3e59107988caeef3518e5872cb8
giluyeh <ahmad.mir.9696@gmail.com>
2025-01-11 21:58:53 +0330

    add test_origin_main

--- python.py
+++ python.py
@@ -12,3 +12,13 @@ def test_config(test):
 def test_fetch(test):
     return test
 
+def test_origin_main(test):
+    if test:
+        return True
+    return False
+
+def test_origin_config(test):
+    if test:
+        return True
+    return False
+
